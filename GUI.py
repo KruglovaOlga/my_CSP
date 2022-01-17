7d40704186632edbd0ae7aab38387d8a1bbd3388
KruglovaOlga <std094996@ac.eap.gr>
2022-01-17 10:31:27 +0200

    widgets_1

--- GUI.py
+++ GUI.py
@@ -2,9 +2,30 @@ from tkinter import *
 
 root = Tk()
 root.title('Cutting Stock Problem Solver')
+root.geometry('940x500')
+root.config(bg='#119ff1')
 
-frame = LabelFrame(root,text = "This is my frame...", padx = 300, pady = 200)
-frame.pack(padx=10, pady=10)
-b = Button(frame,text="Calculate")
-b.pack()
+#widgets
+left_frame = Frame(root, bd=2, relief=SOLID, padx=10, pady=10)
+
+Label(left_frame, text="Insert width of demands", font=('Times', 14)).grid(row=0, column=0, sticky=W, pady=10)
+Label(left_frame, text="Insert quantity", font=('Times', 14)).grid(row=1, column=0, pady=10)
+Label(left_frame, text="Insert width of stock", font=('Times', 14)).grid(row=2, column=0, pady=10)
+demands = Entry(left_frame, font=('Times', 14))
+quantity = Entry(left_frame, font=('Times', 14))
+stock = Entry(left_frame, font=('Times', 14))
+calculate_btn = Button(left_frame, width=15, text='Calculate', font=('Times', 14), command=None)
+
+right_frame = Frame(root, bd=2, relief=SOLID, padx=10, pady=10)
+
+# widgets placement
+demands.grid(row=0, column=1, pady=10, padx=20)
+quantity.grid(row=1, column=1, pady=10, padx=20)
+stock.grid(row=2, column=1, pady=10, padx=20)
+calculate_btn.grid(row=3, column=1, pady=10, padx=20)
+left_frame.place(x=50, y=50)
+
+
+
+# infinite loop
 root.mainloop()
